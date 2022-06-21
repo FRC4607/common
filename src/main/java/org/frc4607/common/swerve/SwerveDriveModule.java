a5808d69ed1eb7771065708979f836cd4bafb4d4
Adam S <majestix123@outlook.com>
2022-06-21 16:19:03 -0500

    add plugins to common library

--- src/main/java/org/frc4607/common/swerve/SwerveDriveModule.java
+++ src/main/java/org/frc4607/common/swerve/SwerveDriveModule.java
@@ -3,44 +3,72 @@ package org.frc4607.common.swerve;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
+/**
+ * Represents one swerve drive module on the robot.
+ */
 public class SwerveDriveModule {
-    private final SwerveMotorBase mDriveMotor;
-    private final SwerveMotorBase mTurnMotor;
-    private final Translation2d mModuleLocation;
+    private final SwerveMotorBase m_driveMotor;
+    private final SwerveMotorBase m_turnMotor;
+    private final Translation2d m_moduleLocation;
 
-    public SwerveDriveModule(SwerveMotorBase drive, SwerveMotorBase turn, Translation2d moduleLocation) {
-        mDriveMotor = drive;
-        mTurnMotor = turn;
-        mModuleLocation = moduleLocation;
+    /**
+     * Constructs a new instance of {@code SwerveDriveModule}.
+     *
+     * @param drive A class that implements {@link org.frc4607.common.swerve.SwerveMotorBase} which
+     controls the drive motor of the swerve module.
+     * @param turn A class that implements {@link org.frc4607.common.swerve.SwerveMotorBase} which
+     controls the turning motor of the swerve module.
+     * @param moduleLocation A {@link edu.wpi.first.math.geometry.Translation2d} in the robot
+     coordinate frame with units in meters representing the location of the swerve module relative
+     to the center of the robot.
+     */
+    public SwerveDriveModule(SwerveMotorBase drive, SwerveMotorBase turn, 
+        Translation2d moduleLocation) {
+        m_driveMotor = drive;
+        m_turnMotor = turn;
+        m_moduleLocation = moduleLocation;
     }
 
+    /**
+     * Sets the swerve module to a given state.
+     *
+     * @param state A {@link edu.wpi.first.math.kinematics.SwerveModuleState} representing
+     the desired state of the swerve module.
+     */
     public void set(SwerveModuleState state) {
-        SwerveModuleState newState = SwerveModuleState.optimize(state, mTurnMotor.getRotation2d());
-        mDriveMotor.setTarget(newState.speedMetersPerSecond);
-        mTurnMotor.setTarget(newState.angle.getDegrees());
+        SwerveModuleState newState = SwerveModuleState.optimize(state, m_turnMotor.getRotation2d());
+        m_driveMotor.setTarget(newState.speedMetersPerSecond);
+        m_turnMotor.setTarget(newState.angle.getDegrees());
     }
+
 
     public double getDriveMotorPosition() {
-        return mDriveMotor.getEncoderPosition();
+        return m_driveMotor.getEncoderPosition();
     }
+
     public double getDriveMotorVelocity() {
-        return mDriveMotor.getEncoderVelocity();
+        return m_driveMotor.getEncoderVelocity();
     }
+
     public double getTurnMotorPosition() {
-        return mTurnMotor.getEncoderPosition();
+        return m_turnMotor.getEncoderPosition();
     }
+
     public double getTurnMotorVelocity() {
-        return mTurnMotor.getEncoderVelocity();
+        return m_turnMotor.getEncoderVelocity();
     }
+
 
     public Translation2d getModuleLocation() {
-        return mModuleLocation;
+        return m_moduleLocation;
     }
 
+
     public boolean isDriveMotorConnected() {
-        return mDriveMotor.isConnected();
+        return m_driveMotor.isConnected();
     }
+
     public boolean isTurnMotorConnected() {
-        return mTurnMotor.isConnected();
+        return m_turnMotor.isConnected();
     }
 }
---
+++ src/test/java/CisLoggerTest.java
@@ -0,0 +1,281 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.RobotController;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.frc4607.common.logging.CisLogger;
+import org.frc4607.common.logging.CisLogger.SubLogger;
+import org.frc4607.common.util.Zip;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+// import org.frc4607.common.logging.LogTest;
+
+/**
+ * Performs unit tests on {@link org.frc4607.common.logging.CisLogger}.
+ */
+@SuppressWarnings({"PMD.SystemPrintln"}) // println is ok here because it is a test
+public class CisLoggerTest {
+    private static final int MESSAGE_ID = 2;
+    private static final String TIMESTAMP = "Timestamp";
+    private static final String CSVDIR = "CSVDIR";
+    private static final String TEST = "Test";
+    private static final String TELEMETRY_TEST = "TelemetryTest";
+    private static final String TE_COMMA_ST = "te,st";
+    private static final String TE_QUOTE_ST = "te\"st";
+    private static final String TEST_LOWERCASE = "test";
+    private static final String ESCAPE_TEST = "EscapeTest";
+
+    /**
+     * Compares a CSV table with a refrence.
+     *
+     * @param input  A CSVParser from a CSV file.
+     * @param target A List containing Lists of Strings corresponding to the cells
+     *               in the table.
+     */
+    public static boolean compareTables(CSVParser input, List<List<String>> target)
+        throws IOException {
+        List<Map.Entry<CSVRecord, List<String>>> rows = Zip.zip(input.getRecords(), target);
+        int line = 0;
+        for (Map.Entry<CSVRecord, List<String>> pair : rows) {
+            List<Map.Entry<String, String>> values
+                = Zip.zip(pair.getKey().toList(), pair.getValue());
+            int entry = 0;
+            for (Map.Entry<String, String> pair2 : values) {
+                if (entry != MESSAGE_ID) {
+                    if (!pair2.getKey().equals(pair2.getValue())) {
+                        System.out.println("On line " + line + " and entry " + entry);
+                        System.out.println("Expected: " + pair2.getValue());
+                        System.out.println("Got: " + pair2.getKey());
+                        return false;
+                    }
+                } else {
+                    if (!(TIMESTAMP.equals(pair2.getKey()))
+                            && !(TIMESTAMP.equals(pair2.getValue()))) {
+                        long inputTime = Long.parseLong(pair2.getKey());
+                        long targetTime = Long.parseLong(pair2.getValue());
+                        if (!((targetTime - 1000000 <= inputTime) 
+                                && (targetTime + 1000000 >= inputTime))) {
+                            System.out.println("Expected: "
+                                + pair2.getValue() + " within 1 second");
+                            System.out.println("Got: " + pair2.getKey());
+                            return false;
+                        }
+                    } else {
+                        if (!pair2.getKey().equals(pair2.getValue())) {
+                            System.out.println("On line " + line + " and entry " + entry);
+                            System.out.println("Expected: " + pair2.getValue());
+                            System.out.println("Got: " + pair2.getKey());
+                            return false;
+                        }
+                    }
+                }
+                entry++;
+            }
+        }
+        return true;
+    }
+
+    /** Cleans up the CSV directory before tests begin. */
+    @BeforeClass
+    public static void cleanFolder() {
+        File csvdir = new File(System.getenv(CSVDIR));
+        File[] files = csvdir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (!file.delete()) {
+                    fail("File delete failed.");
+                }
+            }
+        }
+    }
+
+    /** Initializes the HAL. */
+    @Before
+    public void setupHal() {
+        assert HAL.initialize(500, 0);
+    }
+
+    /**
+     * Tests if the logger will reject names with special characters.
+     */
+    @Test
+    public void testReservedCharacters() {
+        // https://en.wikipedia.org/wiki/Filename#In_Windows
+        String badNames = "/\\?%*:|\"<>.,;= ";
+        for (char badChar : badNames.toCharArray()) {
+            String badString = Character.toString(badChar);
+            try {
+                new CisLogger(badString); // NOPMD: A clean logger is needed to test properly.
+                fail("A Logger with the character " + badString + " was allowed to be created.");
+            } catch (IllegalArgumentException e) {
+                assertEquals("The error message when invalid characters are used failed to match.",
+                    e.getMessage(), "Logger names must be valid Windows filenames.");
+            }
+        }
+    }
+
+    /**
+     * Tests if the logger will prohibit sending telemetry if no labels were given
+     * to it.
+     */
+    @Test
+    public void testEmptyLabels() {
+        try {
+            CisLogger log = new CisLogger(TEST);
+            SubLogger sublog = log.createSubLogger(TEST, new String[] {});
+            sublog.logTelemetry(new Object[] {});
+        } catch (IllegalStateException e) {
+            assertEquals("The error message when trying to use telemetry with no labels failed to " 
+                + "match",
+                e.getMessage(), "Loggers created with no labels cannot send telemetry.");
+        }
+    }
+
+    /**
+     * Tests to see if the logger will reject telemetry data if it contains too few objects.
+     */
+    @Test
+    public void testInvalidDataSize_tooFewObjects() {
+        try {
+            CisLogger log = new CisLogger(TEST);
+            SubLogger sublog = log.createSubLogger(TEST, new String[] { "a", "b", "c" });
+            sublog.logTelemetry(new Object[] { "a" });
+        } catch (IllegalArgumentException e) {
+            assertEquals("The error message produced when giving an invalid number of objects "
+                    + "failed to match", e.getMessage(),
+                    "The number of objects passed to Logger.logTelemetry() must be the " 
+                    + "same as the number of labels that were created.");
+        }
+    }
+
+    /**
+     * Tests to see if the logger will reject telemetry data if it contains too many objects.
+     */
+    @Test
+    public void testInvalidDataSize_tooManyObjects() {
+        try {
+            CisLogger log = new CisLogger(TEST);
+            SubLogger sublog = log.createSubLogger(TEST, new String[] { "a", "b", "c" });
+            sublog.logTelemetry(new Object[] { "a", "b", "c", "d", "e" });
+        } catch (IllegalArgumentException e) {
+            assertEquals("The error message produced when giving an invalid number of objects "
+                    + "failed to match", e.getMessage(),
+                    "The number of objects passed to Logger.logTelemetry() must be the "
+                    + "same as the number of labels that were created.");
+        }
+    }
+
+    /** Tests the logger's ability to write telemetry. */
+    @Test
+    public void testTelemetry() throws FileNotFoundException, IOException, InterruptedException {
+        String time = Long.toString(RobotController.getFPGATime());
+
+        CisLogger log = new CisLogger("0_TelemetryTest");
+        SubLogger sublog = log.createSubLogger(TELEMETRY_TEST, new String[] { "a", "b", "c" });
+        Object[] test1 = new Object[] { 1, 2, 3 };
+        Object[] test2 = new Object[] { "x", "y", "z" };
+        Object[] test3 = new Object[] { false, true, false };
+        Object[] test4 = new Object[] { 1, "y", false };
+        sublog.logTelemetry(test1);
+        sublog.logTelemetry(test2);
+        sublog.logTelemetry(test3);
+        sublog.logTelemetry(test4);
+
+        String[] result0 = new String[] { "0", TELEMETRY_TEST, "Timestamp", "a", "b", "c" };
+        String[] result1 = new String[] { "0", TELEMETRY_TEST, time, "1", "2", "3" };
+        String[] result2 = new String[] { "0", TELEMETRY_TEST, time, "x", "y", "z" };
+        String[] result3 = new String[] { "0", TELEMETRY_TEST, time, "false", "true", "false" };
+        String[] result4 = new String[] { "0", TELEMETRY_TEST, time, "1", "y", "false" };
+        List<List<String>> table = new ArrayList<List<String>>();
+        table.add(Arrays.asList(result0));
+        table.add(Arrays.asList(result1));
+        table.add(Arrays.asList(result2));
+        table.add(Arrays.asList(result3));
+        table.add(Arrays.asList(result4));
+
+        Thread.sleep(500);
+
+        String csvdir = System.getenv(CSVDIR);
+        String filename = csvdir + "/" + "0_TelemetryTest.csv";
+        assertTrue("The tables in the telemetry test failed to match",
+            compareTables(CSVFormat.DEFAULT.parse(Files.newBufferedReader(Paths.get(filename))),
+                table));
+    }
+
+    /** Tests the logger's ability to write messages. */
+    @Test
+    public void testMessages() throws FileNotFoundException, IOException, InterruptedException {
+        String time = Long.toString(RobotController.getFPGATime());
+
+        CisLogger log = new CisLogger("1_MessageTest");
+        SubLogger sublog = log.createSubLogger("MessageTest", new String[] {});
+        sublog.logMessage("Test Message 1");
+        sublog.logMessage("Test Message 2");
+
+        String[] result0 = new String[] { "1", "MessageTest", time, "Test Message 1" };
+        String[] result1 = new String[] { "1", "MessageTest", time, "Test Message 2" };
+        List<List<String>> table = new ArrayList<List<String>>();
+        table.add(Arrays.asList(result0));
+        table.add(Arrays.asList(result1));
+
+        Thread.sleep(500);
+
+        String csvdir = System.getenv(CSVDIR);
+        String filename = csvdir + "/" + "1_MessageTest.csv";
+        assertTrue("The tables in the message test failed to match", compareTables(
+            CSVFormat.DEFAULT.parse(Files.newBufferedReader(Paths.get(filename))), table));
+    }
+
+    /** Tests the logger's ability to escape the characters {@code "} and {@code ,}.*/
+    @Test
+    public void testEscape() throws FileNotFoundException, IOException, InterruptedException {
+        String time = Long.toString(RobotController.getFPGATime());
+
+        CisLogger log = new CisLogger("2_EscapeTest");
+        SubLogger sublog = log.createSubLogger(ESCAPE_TEST,
+            new String[] { TEST_LOWERCASE, TE_COMMA_ST, TE_QUOTE_ST });
+        sublog.logTelemetry(new Object[] { TEST_LOWERCASE, TE_COMMA_ST, TE_QUOTE_ST });
+        sublog.logMessage("This message contains a , and a \".");
+        sublog.logMessage("This message contains no special characters.");
+
+        // Commons CSV will automatically escape for us, so there's no need to put extra
+        // quote marks.
+        String[] result0 = new String[] { "0", ESCAPE_TEST, "Timestamp",
+            TEST_LOWERCASE, TE_COMMA_ST, TE_QUOTE_ST };
+        String[] result1 = new String[] { "0", ESCAPE_TEST, time, TEST_LOWERCASE,
+            TE_COMMA_ST, TE_QUOTE_ST };
+        String[] result2 = new String[] { "1", ESCAPE_TEST, time, "This message "
+                + "contains a , and a \"." };
+        String[] result3 = new String[] { "1", ESCAPE_TEST, time,
+            "This message contains no special characters." };
+        List<List<String>> table = new ArrayList<List<String>>();
+        table.add(Arrays.asList(result0));
+        table.add(Arrays.asList(result1));
+        table.add(Arrays.asList(result2));
+        table.add(Arrays.asList(result3));
+
+        Thread.sleep(500);
+
+        String csvdir = System.getenv(CSVDIR);
+        String filename = csvdir + "/" + "2_EscapeTest.csv";
+        assertTrue("The tables in the escape test failed to match",
+            compareTables(CSVFormat.DEFAULT.parse(
+                Files.newBufferedReader(Paths.get(filename))), table));
+    }
+}
